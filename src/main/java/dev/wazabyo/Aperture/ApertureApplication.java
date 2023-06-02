05f1fb8a65aadf99be0f6431dba4a43daa3c52e4
haiseong <wjdgotjd9908@gmail.com>
2023-06-02 18:21:18 +0900

    init

---
+++ src/main/java/dev/wazabyo/Aperture/ApertureApplication.java
@@ -0,0 +1,13 @@
+package dev.wazabyo.Aperture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApertureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApertureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wazabyo/Aperture/ApertureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wazabyo.Aperture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApertureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
