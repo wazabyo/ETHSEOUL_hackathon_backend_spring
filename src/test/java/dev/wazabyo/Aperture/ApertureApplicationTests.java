07caa8534bbe06a6c59915739516d74a17148525
haiseong <wjdgotjd9908@gmail.com>
2023-06-02 18:57:31 +0900

    [ci] test

--- src/test/java/dev/wazabyo/Aperture/ApertureApplicationTests.java
+++ src/test/java/dev/wazabyo/Aperture/ApertureApplicationTests.java
@@ -2,12 +2,15 @@ package dev.wazabyo.Aperture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class ApertureApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test1() {
+		Assertions.assertEquals("test", "test");
 	}
 
 }
